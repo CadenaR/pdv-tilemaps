75b816b758a41773f39dde507708a0289491afd3
CadenaR <a01652141@itesm.mx>
2019-04-15 15:43:03 -0500

    Rigid body and more

---
+++ Assets/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform Player;
+    public string tempJson = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SaveAction()
+    {
+        PlayerData data = new PlayerData();
+        data.position = Player.position;
+        data.life = 75;
+        string dataString = JsonUtility.ToJson(data);
+        tempJson = dataString;
+        Debug.Log("hola");
+        Debug.Log("Data string" + dataString);
+    }
+
+    public void LoadAction()
+    {
+        string dataString = tempJson;
+        Debug.Log(dataString);
+        PlayerData data = JsonUtility.FromJson<PlayerData>(dataString);
+        Player.position = data.position;
+    }
+}
+
+public class PlayerData
+{
+    public Vector3 position;
+    public int life;
+
+}
